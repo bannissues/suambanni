50f4ed49a2ef25211915ccb1df30f12e214dbce2
banni <124708893+bannissues@users.noreply.github.com>
2025-08-22 23:01:00 +0000

    html separado do php por meio do "action" no form

--- php/ex1.php
+++ php/ex1.php
@@ -9,7 +9,7 @@
     <h1>student's grades</h1>
 
     <!--formulário com método post para calcular a média com o - tambem - método post no PHP -->
-    <form action="" method="post">
+    <form action="ex1back.php" method="post">
         <label for="name">student's name: </label>
         <input type="text" name="name" id="name">
 <br>
@@ -27,36 +27,6 @@
 <br>
         <input type="submit" value="what's my average? "></label>
     </form>
-
-<?php 
-
-//verifica se o formulario foi enviado
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $grade1 = $_POST["grade1"];
-    $grade2 = $_POST["grade2"];
-
-    //calcula a média
-    $media = ($grade1 + $grade2) / 2;
-
-    //exibe o resulado
-    echo "<h2>resultado: </h2>";
-    echo "student: <strong>$name</strong><br>";
-    echo "media: <strong>$media</strong>";
-}
-
-/*
-//readline espera um valor e atribui à variável
-$name = readline("type your name: ");
-$grade1 = readline("exam grade: ");
-$grade2 = readline("second exam grade: ");
-
-$media = ($grade1 + $grade2) / 2;
-//as aspas simples mostram exatamente o que foi escrito, já as aspas normais mostram textos separados de variáveis
-echo "your media is $media";
-*/
-
-?>
 
 </body>
 </html>
